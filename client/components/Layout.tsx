c93ffcdfccc91ec5dc4fa38a2b117ece309b6cfa
Nitish Kumar <kumarnitish072001@gmail.com>
2023-08-15 16:51:12 +0530

    :added colors

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
 interface LayoutProps {
-  children: React.ReactNode; // Specify the type of 'children' prop
+  children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <div className="container mx-auto p-4">{children}</div>
+    <div className="bg-gradient-to-b from-blue-500 to-purple-500 min-h-screen flex flex-col">
+      <header className="bg-white py-4 shadow-md">
+        <div className="container mx-auto text-center text-2xl font-semibold">
+          Train Status Checker
+        </div>
+      </header>
+      <main className="flex-grow container mx-auto p-4">{children}</main>
+      <footer className="bg-gray-200 py-4">
+        <div className="container mx-auto text-center text-sm text-gray-600">
+          © 2023 Train Status Checker. All rights reserved.
+        </div>
+      </footer>
     </div>
   );
 };
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,14 +8,15 @@ interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = ({ trains }) => {
-  console.log(trains);
   return (
     <Layout>
-      <h1 className="text-2xl font-bold mb-4">All Trains</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {trains?.map((train) => (
-          <TrainCard key={train?.trainNumber} train={train} />
-        ))}
+      <div className="p-6 rounded-xl bg-white shadow-md">
+        <h1 className="text-2xl font-bold mb-4">All Trains</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {trains?.map((train) => (
+            <TrainCard key={train?.trainNumber} train={train} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
@@ -23,7 +24,6 @@ const Home: React.FC<HomeProps> = ({ trains }) => {
 
 export async function getServerSideProps() {
   const response = await axios.get("http://localhost:8000/trains");
-  console.log(response);
   const trains: any[] = response.data;
   return { props: { trains } };
 }
